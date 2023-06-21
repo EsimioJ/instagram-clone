a711168b48cd86ebc939f6211b006a6a4d727b70
Jacopo Riccioni <EsimioJ@users.noreply.github.com>
2023-06-21 19:34:35 +0200

    tracks - zone relationship
    Co-authored-by: Alessandro <alevann@users.noreply.github.com>

--- components/Trecks.js
+++ components/Trecks.js
@@ -1,5 +1,6 @@
 import { db } from "@/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, getDoc } from "firebase/firestore";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 const Trecks = () => {
@@ -9,22 +10,45 @@ const Trecks = () => {
     const fetchTracks = async () => {
       const tracksCollection = collection(db, "tracks");
       const tracksSnapshot = await getDocs(tracksCollection);
-      const tracksData = tracksSnapshot.docs.map((doc) => ({
+      const tracks = tracksSnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
-      setPiste(tracksData);
+
+      await Promise.all(
+        tracks.map(async (track) => {
+          const zona = await getDoc(track.zona);
+          track.zona = {
+            id: zona.id,
+            ...zona.data(),
+          };
+        })
+      );
+
+      setPiste(tracks);
     };
 
     fetchTracks();
   }, []);
 
+  console.log(piste);
+
   return (
     <div>
+        <div className="grid grid-cols-3 gap-4 font-bold">
+            <span>Tracciati</span>
+            <span>Zone</span>
+            <span>Stato</span>
+        </div>
       {piste.map((track) => (
-        <div key={track.id}>
-          <h1>{track.slug}</h1>
-          <h1>{track?.zona}</h1>
+        <div key={track.id} className="grid grid-cols-3 gap-4">
+          <Link href={`/tracks/${track.slug}`}>
+            <span>{track.num}</span> -<span> {track.title}</span>
+          </Link>
+          <Link href={`/zone/${track.zona.slug}`}>
+            <span>{track.zona.nome}</span>
+          </Link>
+          <span>{track.stato}</span>
         </div>
       ))}
     </div>
