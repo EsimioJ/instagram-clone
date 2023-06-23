e64c04c2b90f5a39cc2b2d80148c555b8a1e69cf
Jacopo Riccioni <EsimioJ@users.noreply.github.com>
2023-06-23 22:08:39 +0200

    not set

--- pages/tracks/[title].js
+++ pages/tracks/[title].js
@@ -13,25 +13,18 @@ const Zone = () => {
   console.log(slug);
   console.log(router);
 
+  
+  
+  const stati = ["OK","KO", "Warn"]
+  
+  const [track, setTrack] = useState({
+    title:"not set",
+    num:"not set",
+    slug:"",
+  });
   const q = query(collection(db, "tracks"), where("slug", "==", slug));
 
-
-  const stati = ["OK","KO", "Warn"]
-
-  const [track, setTrack] = useState();
-
   useEffect(() => async () => {
-    //   const docRef = doc(db, "tracks", "cuplina");
-    //   const docSnap = await getDoc(docRef);
-
-    //   if (docSnap.exists()) {
-    //     console.log("Document data:", docSnap.data());
-    //     setTrack(docSnap.data())
-    //   } else {
-    //     // docSnap.data() will be undefined in this case
-    //     console.log("No such document!");
-    //   }
-    // };
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
